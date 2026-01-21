f0e537c4539b5503223925c622a6bda356c40f29
ch1m3z13 <chimeo81@gmail.com>
2026-01-21 19:41:26 +0100

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import GhostProtocolApp from './App.jsx' // Updated name
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GhostProtocolApp /> 
   </StrictMode>,
 )
